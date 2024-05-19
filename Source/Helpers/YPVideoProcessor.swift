d0247afb40320c8ce3c082280060a7e084d60c40
kane-liu <handsome.liu1986@gmail.com>
2024-05-19 22:21:42 +0900

    Fix url

--- Source/Helpers/YPVideoProcessor.swift
+++ Source/Helpers/YPVideoProcessor.swift
@@ -22,8 +22,9 @@ class YPVideoProcessor {
     static func makeVideoPathURL(temporaryFolder: Bool, fileName: String) -> URL {
         var outputURL: URL
         
+        let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.path ?? NSTemporaryDirectory()
         if temporaryFolder {
-            let outputPath = "\(NSTemporaryDirectory())\(fileName).\(YPConfig.video.fileType.fileExtension)"
+            let outputPath = "\(path)\(fileName).\(YPConfig.video.fileType.fileExtension)"
             outputURL = URL(fileURLWithPath: outputPath)
         } else {
             guard let documentsURL = FileManager
